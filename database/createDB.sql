0e941bcec257974301341c599fb91fc61c64d0dd
andre.andrade@conab.gov.br <andre.andrade@conab.gov.br>
2022-07-20 01:50:42 -0300

    Going to angular 13 and electron 18

--- database/createDB.sql
+++ database/createDB.sql
@@ -10,55 +10,52 @@ CREATE TABLE tb_environ (
 INSERT INTO tb_environ(db_version)
 VALUES('0.0.1');
 
+/* Table Game Type
+ * Registers the type of game.
+ * ex.: videogame, board game, etc.
+ */
+CREATE TABLE tb_game_type (
+  id INTEGER PRIMARY KEY autoincrement,
+  type TEXT not null UNIQUE,
+  icon_path TEXT,
+  description TEXT,
+  has_platform INTEGER not null DEFAULT 0 -- Boolean
+)
+
 /* Table Platform
- * Register the hardware/O.S. platform where the game runs.
- * ex: PS4, Windows, Linux, DS, etc.
+ * Registers platform where the game you have is in.
+ * ex.: PS4, Steam, PSN, Physical Media, cartdrige, etc.
  */
 CREATE TABLE tb_platform (
   id INTEGER PRIMARY KEY autoincrement,
-  name TEXT not null UNIQUE,
-  icon_path TEXT not null UNIQUE,
-  creation_date TEXT not null DEFAULT (date('now')),
-  last_alteration_date TEXT
+  name TEXT not null UNIQUE
 );
 
-/* Table Media
- * Register the media the game is distributed.
- * ex: PSN, Blue Ray, Steam, Cartridge, etc.
+/* Table Game
+ * This is the game per se.
  */
-CREATE TABLE tb_media (
-  id INTEGER PRIMARY KEY autoincrement,
-  name TEXT not null UNIQUE,
-  icon_path TEXT not null UNIQUE,
-  creation_date TEXT not null DEFAULT (date('now')),
-  last_alteration_date TEXT
-);
-
-/* Table Title
- * This is the game title. The game can have many copies of a title, in many
- * platforms and medias.
- */
-CREATE TABLE tb_title (
+CREATE TABLE tb_game (
   id INTEGER PRIMARY KEY autoincrement,
   name TEXT not null,
-  dlc INTEGER not null DEFAULT 0, -- Boolean
-  dlc_title_fk INTEGER REFERENCES tb_title(id) ON DELETE cascade,
+  game_type_fk INTEGER REFERENCES tb_game_type(id) ON DELETE restrict,
+  is_dlc_expansion INTEGER not null DEFAULT 0, -- Boolean
+  dlc_expansion_game_fk INTEGER REFERENCES tb_title(id) ON DELETE cascade,
+  cover_box_art_path TEXT,
   creation_date TEXT not null DEFAULT (date('now')),
   last_alteration_date TEXT,
-  CONSTRAINT dlc_check CHECK ((dlc = 0 AND dlc_title_fk IS NULL) OR
-                              (dlc = 1 AND dlc_title_fk IS NOT NULL))
+  CONSTRAINT dlc_expansion_check CHECK 
+    ((is_dlc_expansion = 0 AND dlc_expansion_game_fk IS NULL) OR
+     (is_dlc_expansion = 1 AND dlc_expansion_game_fk IS NOT NULL))
 );
 
-/* Table Copy
- * This represents a copy of a title in a single platform and media.
+/* Table Game Platform
+ * N to N between game and platform. You can own the same game in many 
+ * platforms.
  */
-CREATE TABLE tb_copy (
-  id INTEGER PRIMARY KEY autoincrement,
-  title_fk INTEGER not null REFERENCES tb_title(id) ON DELETE cascade,
-  platform_fk INTEGER not null REFERENCES tb_platform(id) ON DELETE cascade,
-  media_fk INTEGER not null REFERENCES tb_media(id) ON DELETE cascade,
-  cover_path TEXT not null,
-  creation_date TEXT not null DEFAULT (date('now')),
-  last_alteration_date TEXT,
-  CONSTRAINT copy_ak UNIQUE (title_fk, platform_fk, media_fk)
-);
+CREATE TABLE tb_game_platform (
+  game_fk INTEGER REFERENCES tb_game(id) ON DELETE restrict,
+  platform_fk INTEGER REFERENCES tb_platform(id) ON DELETE restrict,
+  CONSTRAINT game_platform_pk PRIMARY KEY (game_fk, platform_fk)
+)
+
+-- shelves
